a85f77e3d875e8bb650422e92c805b73a799c4e8
ubaidullah-code <ubaidque14@gmail.com>
2025-12-11 00:14:29 +0500

    another

--- practise/test.php
+++ practise/test.php
@@ -1,3 +1,18 @@
+<html>
+    <head>
+        <title>First code in php</title>
+    </head>
+    <body>
+      <h1 style="text-align:center; background: red; margin:0"><u>first code in Php</u></h1>  
+    </body>
+</html>
+
+
 <?php
-echo "welcome to php course"
+    $a = 100;
+    $b = 50;
+    $c = true;
+echo "welcome to php course<br/>";
+echo "check with html tag";
+echo '<br>value a is '.$a.' and value b is '.$b.' value c is '.$c;
 ?>
