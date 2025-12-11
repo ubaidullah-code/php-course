cb4a149a9215f14092816885f31ccf43759b4bdd
ubaidullah-code <ubaidque14@gmail.com>
2025-12-11 12:47:16 +0500

    check

--- practise/test.php
+++ practise/test.php
@@ -1,9 +1,17 @@
 <html>
     <head>
         <title>First code in php</title>
+        <style>
+            *{
+                margin: 0;
+                padding: 0;
+                font-family:sans-serif;
+                text-align:center;
+            }
+        </style>
     </head>
     <body>
-      <h1 style="text-align:center; background: red; margin:0"><u>first code in Php</u></h1>  
+      <h1 style="text-align:center; background: red;"><u>first code in Php</u></h1>  
     </body>
 </html>
 
@@ -15,4 +23,16 @@
 echo "welcome to php course<br/>";
 echo "check with html tag";
 echo '<br>value a is '.$a.' and value b is '.$b.' value c is '.$c;
+
+            // data type in Php
+            /*string
+            integer
+            booleam
+            float
+            array
+            object
+            null
+            resoure
+            */
+            
 ?>
