e59ae22a5c3004f467858968a108c35dc220d8ed
ubaidullah-code <ubaidque14@gmail.com>
2025-12-29 01:04:44 +0500

    traits

---
+++ OOPS/traits.php
@@ -0,0 +1,66 @@
+<?php
+
+echo "<center><h1>traits</h1></center>";
+?>
+ <!--polymorphison definition -->
+<!-- Same operation may be behave differently in different classes -->
+
+<?php 
+// use trait if you don't shared inheritance data with very childs
+    trait A{
+        function fun1(){
+            echo "fun1<br>";
+        }
+    }
+    class B {
+        // using trait A class using "use" keyword
+        use A;
+        function fun2(){
+            echo "fun2<br>";
+        }
+    }
+    class C extends B{
+        function fun3(){
+            echo "fun3<br>";
+        }
+    }
+    $obj = new C();
+
+    $obj->fun1();
+    $obj->fun2();
+
+?>
+
+<?php  
+
+trait message1{
+    public function msge(){
+        echo "OOPS it's fun!";
+    }
+}
+    trait message2{
+        public function msge(){
+            echo "OOPS reduces the code duplication!";
+        } 
+    }
+    class welcome1{
+        use message1;
+    }
+    class welcome2{
+        use message1, message2{
+            message1::msge insteadof message2;
+            message2::msge as msge2;
+        }
+
+    }
+    $wel1 = new welcome1();
+    $wel1->msge();
+    echo "<br>";
+    $wel2 =new welcome2();
+    $wel2->msge();
+    $wel2->msge2();
+
+
+
+?>
+ 
