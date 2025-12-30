81623f5384d65ffd659c504d74e6a42cc4d61086
ubaidullah-code <ubaidque14@gmail.com>
2025-12-30 15:42:12 +0500

    insert query

---
+++ database/databaseQuery.php
@@ -0,0 +1,63 @@
+<?php
+
+
+include "./config/databaseConnection.php"
+    ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+//     $employee_id = 23;
+//     $employee_name = "ubaidullah";
+//     $employee_salary = 50000;
+//     $department = "IT";
+//     $sqlQuery = "insert into employee values(?,?,?,?)";
+//     $ps = $conn->prepare($sqlQuery);
+//     $ps->bind_param('isis', $employee_id,$employee_name,$employee_salary,$department);
+//     $ps->execute();
+//     echo "Record inserted in the table";
+    
+?>
+
+<?php 
+
+
+//with direct store;
+
+// $sqlQuery = "insert into users (userName , password ) values ('qadir', 'pass123')";
+
+//     $result = $conn->query($sqlQuery);
+//     if ($result) {
+//         echo "<br>result in recorded";
+//     }
+//     else{
+//         echo "result is not recorded";
+//     }
+
+
+// with variable store / secure method 
+
+$name = 'ubaidullah';
+$pass = 'variablepass';
+$sqlQuery = "insert into users (userName , password ) values (? , ?)";
+
+    $result = $conn->prepare($sqlQuery);
+    $result->bind_param('ss', $name, $pass);
+    $result->execute();
+    if ($result) {
+        echo "<br>variable with query is added record";
+    }
+    else{
+        echo "<br> variable with query is not added record $conn->error";
+    }
+   
+?>
+</body>
+</html>
