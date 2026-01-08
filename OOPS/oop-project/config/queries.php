d3ce74facefe900d203a3826bedbbdb8b536c031
ubaidullah-code <ubaidque14@gmail.com>
2026-01-09 01:06:02 +0500

    dynamic update query

--- OOPS/oop-project/config/queries.php
+++ OOPS/oop-project/config/queries.php
@@ -5,46 +5,67 @@ include_once "databaseConnection.php";
 <?php
 class Queries extends database
 {
-    // get query
-    public function get_data($tableName, $id = null , $fields = "*", $id_field = "user_id")
-    {
-        $sql =  $id? "SELECT $fields FROM $tableName WHERE $id_field = $id" : "SELECT $fields FROM $tableName";
-        $result = $this->connect()->query($sql);
-        return $result;
-    }
-    // insert query
-    public function insert_data($tableName, $data)
-    {
-           //split with coma ","
-        $fields = implode(',',array_keys($data));
-        // count values in query with "?"
-        $values = implode(', ', array_fill(0, count($data), "?"));
+  // get query           //tablename , userid     ,  all select  ,  id_format
+  public function get_data($tableName, $id = null, $fields = "*", $id_field = "user_id")
+  {
+    $sql =  $id ? "SELECT $fields FROM $tableName WHERE $id_field = $id" : "SELECT $fields FROM $tableName";
+    $result = $this->connect()->query($sql);
+    return $result;
+  }
+  // insert query
+  public function insert_data($tableName, $data)
+  {
+    //split with coma ","
+    $fields = implode(',', array_keys($data));
+    // count values in query with "?"
+    $values = implode(', ', array_fill(0, count($data), "?"));
 
-         $sql = "INSERT INTO $tableName ($fields)  VALUES ($values)";
+    $sql = "INSERT INTO $tableName ($fields)  VALUES ($values)";
 
-        $result = $this->connect()->prepare($sql);
-         //checking datatype of values
-        $strtype = "";
-        foreach ($data as  $value)
-        {
-          $strtype .= is_int($value)?"i": "s";
-        }
-       
-        
-        // passing the values
-        $result->bind_param($strtype,...array_values($data));
-        $result->execute();
+    $result = $this->connect()->prepare($sql);
+    //checking datatype of values
+    $strtype = "";
+    foreach ($data as  $value) {
+      $strtype .= is_int($value) ? "i" : "s";
     }
 
-    // DELETE QUERY
-      public function delete_data($tableName, $id_field = "user_id", $id)
-    {
-        $sql = "DELETE FROM $tableName WHERE $id_field = ?";
-        $result = $this->connect()->prepare($sql);
-        $result->bind_param('i', $id);
-          $result->execute();
-        
-    }
+
+    // passing the values
+    $result->bind_param($strtype, ...array_values($data));
+    $result->execute();
+  }
+
+  // DELETE QUERY
+  public function delete_data($tableName, $id_field = "user_id", $id)
+  {
+    $sql = "DELETE FROM $tableName WHERE $id_field = ?";
+    $result = $this->connect()->prepare($sql);
+    $result->bind_param('i', $id);
+    $result->execute();
+  }
+  // update QUERY
+  public function update_data($tableName, $fields, $id, $id_field = "user_id")
+  {
+    // set fields with coma and seperation
+    $column =  implode( " = ?,", array_keys($fields) ). " = ?";;
+
+    // check data type 
+    $valType = str_repeat('s', count($fields)) . "i";
+
+    // sql query
+    $sql = "UPDATE  $tableName SET $column  WHERE $id_field = ?";
+    $result = $this->connect()->prepare($sql);
+    //getting array values
+    $values = array_values($fields);
+    // add id in array of values
+    $values[] = $id;
+
+   
+    // exit;
+    // Bind dynamically
+    $result->bind_param($valType, ...$values );
+    $result->execute();
+  }
 }
 
 
--- OOPS/oop-project/execution.php
+++ OOPS/oop-project/execution.php
@@ -10,7 +10,7 @@ $obj = new Queries();
 
 // dummy data
 $data = array(
-    'userName'=> 'Amir khan',
+    'userName'=> 'fahad mustafa',
     'password'=> 'pass1234'
 ); 
 $id = 26;
@@ -18,21 +18,26 @@ $tableName = "users";
 
 // $result = $obj->delete_data($tableName,"user_id",$id);
 
+// update 
+$obj->update_data($tableName, $data , $id);
 
-$result =  $obj->get_data($tableName , $id);
+
+$result =  $obj->get_data($tableName , $id, "*" , "user_id");
+                        //tablename , userid , all select , id_format
 $result =$result->fetch_assoc();
 echo "<pre>";
+// print_r($result);
 
 
 // store user data in session
-session_start();
-$_SESSION['user']= $result;
-print_r($_SESSION['user']);
+// session_start();
+// $_SESSION['user']= $result;
+// print_r($_SESSION['user']);
 
 //cookie
-setcookie("users",json_encode($result));
-$user =  json_decode($_COOKIE['users'], true);
-print_r($user);
+// setcookie("users",json_encode($result));
+// $user =  json_decode($_COOKIE['users'], true);
+// print_r($user);
 
 
 
@@ -42,11 +47,11 @@ print_r($user);
 // $obj->insert_data($tableName, $data);
 
 // calling get query 
-$res = $obj->get_data($tableName , );
+$res = $obj->get_data($tableName);
 if ($res->num_rows > 0) {
     while ($user = $res->fetch_assoc()) {
         echo "<pre>";
-        print_r($user);
+        print_r($user); 
     }
 }
 
